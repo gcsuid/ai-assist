704eeb9ee1d108317e18e742747ad189aefd479a
Ayush <dasayush483@gmail.com>
2025-06-24 17:16:14 +0530

    43:20, basic setting up

---
+++ models/ticket.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { assign } from "nodemailer/lib/shared";
+
+const ticketSchema = new mongoose.Schema({
+
+    title:String,
+    description: String,
+    status: {
+        type: String,
+        default: "TODO",
+        
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        //required: true
+    },
+    assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: null
+    },
+    priority:String,
+    deadline:Date,  
+    helpfulNotes: String,
+    relatedSkills: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },      
+});
+
+export default mongoose.model("Ticket", ticketSchema);
